c6594bb6bb6f3ae20c4b2044e18c36d51521af6e
imt-eiaz <imteiaz@gmail.com>
2025-09-12 15:58:52 +0100

    Finished Project 16. PropTypes

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ root.render(
   <React.StrictMode>
     {/* <App /> */}
     <StarRating
-      maxRating={5}
+      maxRating={"5"}
       messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
     />
     <StarRating size={24} color="red" className="test" defaultRating={2} />
