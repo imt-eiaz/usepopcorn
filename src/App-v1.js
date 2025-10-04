daed3499c50b6f4bb08405bdc5c5798e02ed431b
imt-eiaz <imteiaz@gmail.com>
2025-10-04 14:45:56 +0100

    3. How NOT to Fetch Data in React

--- src/App-v1.js
+++ src/App-v1.js
@@ -50,17 +50,11 @@ const tempWatchedData = [
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
-const KEY = "8b354d4f";
-
 export default function App() {
-  const [movies, setMovies] = useState([]);
-  const [watched, setWatched] = useState([]);
+  const [movies, setMovies] = useState(tempMovieData);
+  const [watched, setWatched] = useState(tempWatchedData);
 
-  fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`)
-    .then((res) => res.json())
-    .then((data) => console.log(data.Search));
-
-  // console.log(movies.length);
+  console.log(movies.length);
   return (
     <>
       <NavBar>
